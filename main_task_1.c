46bd9d71a325886d1a7a0d152f227035da6d60a0
Leo Goutte <leo.goutte@gmail.com>
2020-01-13 16:03:51 -0500

    Finished task 1 & started task 2

--- main_task_1.c
+++ main_task_1.c
@@ -16,9 +16,9 @@ int main()
  u = vector_malloc(mmax);
  mtx = mtx_malloc(mmax, nmax);
 
- for(m=0; m<mmax; m++) 
+ for(m=0; m<mmax; m++)
   {
-   for(n=0; n<nmax; n++) 
+   for(n=0; n<nmax; n++)
     {
      mtx[m][n] = 1.0/(m + n + 1.0); // some number 
     }
@@ -33,7 +33,8 @@ int main()
   }
  fclose(output);
 
- output = fopen("GOUTTE_test2.dat","w"); m = 20.0;
+ output = fopen("GOUTTE_test2.dat","w"); m = 20.0; // set m=20.0 to be able to
+                                                   // plot in 2d
  for(n=0; n<nmax; n++)
   {
    fprintf(output, "%d %e %e\n", n, mtx[m][n], 1.0/(m + n + 1.0));
